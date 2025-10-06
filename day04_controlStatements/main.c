03926b464db1a7db4a8381109db0ee5dfac394b6
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-10-06 23:00:13 +0530

    conditional statements updated

--- day04_controlStatements/main.c
+++ day04_controlStatements/main.c
@@ -1,12 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int num1 = 20 , num2 = 10 ;
+    int num, num1 = 20, num2 = 10, num3 = 15;
 
-    if (num1>num2)
+    // IF STATEMENT
+
+    if (num1 > num2)
     {
         printf("\nFirst Number is Greater ");
     }
+
+    // IF - ELSE STATEMENT
+
+    if (num2 > num3)
+    {
+        printf("\nSecond Number is Greater ");
+    }
+    else
+    {
+        printf("\nThird Number is Greater ");
+    }
+
+    // ELSE - IF STATEMENT
+
+    printf("\nEnter any Number: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+    {
+        printf("\n Number is Positive!");
+    }
+    else if (num < 0)
+    {
+        printf("\n Number is Negative!");
+    }
+    else
+    {
+        printf("\n Number is zero!");
+    }
+
     return 0;
 }
